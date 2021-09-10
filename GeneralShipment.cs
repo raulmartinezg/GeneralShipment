68e909054d1bf0c1c8e9b28fd5e7cb1af7292e9b
raulmartinezg <raul.martinez@edsgroup.us>
2021-09-10 10:59:51 -0500

    General Shipment

---
+++ GeneralShipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Net;
+using Microsoft.SqlServer.Server;
+
+public partial class StoredProcedures
+{
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void GeneralShipment ()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"http://192.168.25.9/LinkOtm/api/OracleLink/GetShipment");
+        request.Method = "GET";
+        request.ContentLength = 0;
+        request.Credentials = CredentialCache.DefaultCredentials;
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        response.Close();
+    }
+}
